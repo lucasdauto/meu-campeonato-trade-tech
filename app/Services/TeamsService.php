9eb7faab19a16e906e8fb1fb0da842f0db770699
Lucas Dauto <lucasdauto@gmail.com>
2024-06-05 14:28:36 -0300

    service layer

---
+++ app/Services/TeamsService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use stdClass;
+
+class TeamsService
+{
+    protected $repository;
+    public function __construct()
+    {
+    }
+
+    public function getAll(string $filter = null): array
+    {
+
+        return $this->repository->getAll($filter);
+    }
+
+    public function findOne(string $id): stdClass | null
+    {
+        return $this->repository->findOne($id);
+    }
+
+    public function delete(string $id): void
+    {
+        $this->repository->delete($id);
+    }
+
+    public function new(string $name): stdClass
+    {
+        return $this->repository->create($name);
+    }
+
+    public function update(string $name, string $id): stdClass | null
+    {
+        return $this->repository->update($name, $id);
+    }
+}
