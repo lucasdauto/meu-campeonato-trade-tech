1ed9720dd885565d7f42ebaf79fe5fed89a03157
Lucas Dauto <lucasdauto@gmail.com>
2024-06-05 14:59:50 -0300

    data in DTO

--- app/DTO/Match/CreateMatchDTO.php
+++ app/DTO/Match/CreateMatchDTO.php
@@ -5,6 +5,10 @@ namespace App\DTO\Match;
 class CreateMatchDTO
 {
     public function __construct(
-        public string $name
+        public int|string $team_a_id,
+        public int|string $team_b_id,
+        public int $team_a_score,
+        public int $team_b_score,
+        public int|string $championship_id,
     ) {}
 }
--- app/DTO/Match/UpdateMatchDTO.php
+++ app/DTO/Match/UpdateMatchDTO.php
@@ -4,5 +4,11 @@ namespace App\DTO\Match;
 
 class UpdateMatchDTO
 {
-    public function __construct(public string $name){}
+    public function __construct(
+        public int|string $team_a_id,
+        public int|string $team_b_id,
+        public int $team_a_score,
+        public int $team_b_score,
+        public int|string $championship_id,
+    ){}
 }
