147b211347406f6856640b5cdc358c1be9b765cc
Lucas Dauto <lucasdauto@gmail.com>
2024-06-07 01:38:52 -0300

    fix title welcome

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -20,7 +20,7 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Welcome" />
+    <Head title="Meu campeonato" />
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
